eeec9802e5a63e5f7362bd9a3d92f0f43b3ad475
junshock5 <topojs@gmail.com>
2025-01-12 23:25:11 +0900

    Player

---
+++ vampire/Assets/Codes/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVector;    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+    }
+}
