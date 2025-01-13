0d41a18016f6b6250ba1deb3ff82e9f9376b6117
junshock5 <topojs@gmail.com>
2025-01-13 23:04:36 +0900

    add animation player

--- vampire/Assets/Codes/Player.cs
+++ vampire/Assets/Codes/Player.cs
@@ -1,18 +1,48 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public Vector2 inputVector;    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+    // 캐릭터 이동방법 3가지
+    // 1. Transform.Translate
+    // 2. Rigidbody.AddForce
+    // 3. Rigidbody.MovePosition
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
+        Vector2 nextVector = inputVector * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + nextVector);
     }
+
+    void OnMove(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVector.magnitude);
+        if (inputVector.x != 0)
+        {
+            spriteRenderer.flipX = inputVector.x < 0;
+        }
+    }   
 }
